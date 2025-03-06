b5b626c19073366d3c647fbe42b0fb07581a0f0d
steffyj987harmon <steffyj987harmon@gmx.com>
2025-03-06 06:32:23 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int c = 2;
+    int d = 3;
+    int e = 4;
+    int f = 7;
+    int g = 11;
+    int h = 9;
+    int i = 6;
+    int j = 8;
+
+    printf("The sum of %d and %d is %d\n", a, b, a + b);
+    printf("The difference between %d and %d is %d\n", c, d, c - d);
+    printf("The product of %d and %d is %d\n", e, f, e * f);
+    printf("The quotient of %d and %d is %d\n", g, h, g / h);
+    printf("The remainder when %d is divided by %d is %d\n", i, j, i % j);
+
+    return 0;
+}
