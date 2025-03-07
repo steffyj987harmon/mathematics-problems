851940d197c48602f5a67f89d0eaad7de3dc2157
steffyj987harmon <steffyj987harmon@gmx.com>
2025-03-07 11:55:46 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+    int x = 5;
+    if (x > 0) {
+        printf("x is greater than zero\n");
+    } else {
+        printf("x is not greater than zero\n");
+    }
+    return 0;
+}
